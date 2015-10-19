c1a8c4119bd6dc6f958b30f28a7960c7107ff8c9
Andy Smith <andjsmit@iupui.edu>
2015-10-19 08:43:03 -0400

    ScramblePattern converted to Scramble subclass. ScaramblePattern descramble fixed.

--- scramble_pattern.rb
+++ scramble_pattern.rb
@@ -1,18 +1,41 @@
-# Class to find words in a string of letters with know spaces
+# Class to find words in a string of letters with know space locations
 
 require_relative 'scramble.rb'
 
-class ScramblePattern
+class ScramblePattern < Scramble
   
-  attr_reader :letters, :spaces
+  attr_reader :letters, :splits
   
-  def initialize(letters, spaces)
-    @letters = letters
-    @spaces = spaces
+  def initialize(letters, splits)
+    super(letters)
+    @splits = splits
+  end
+  
+  def variant (var_num)
+    var_str = super(var_num)
+    phrase = var_str
+    pos = 0
+    @splits.each.with_index do |wrd_count, n|
+      pos = pos + wrd_count + n
+      phrase = phrase.insert(pos, " ") if @splits.size > n + 1
+    end
+    return phrase
   end
   
   def descramble
-    
+    speller = FFI::Aspell::Speller.new('en_US')
+    phrases = Array.new()
+    0.upto(@variant_count - 1) do |i|
+      phrase = variant(i)
+      phrase_words = phrase.split(" ")
+      phrase_pass = true
+      phrase_words.each do |wrd|
+        phrase_pass = false if !speller.correct?(wrd)
+      end
+      phrases << phrase if phrase_pass && !phrases.include?(phrase)
+    end
+    speller.close
+    return phrases
   end
   
 end
--- test_scramble_pattern.rb
+++ test_scramble_pattern.rb
@@ -4,7 +4,7 @@ require 'test/unit'
 class TestScramblePattern < Test::Unit::TestCase
   
   def test_pattern
-    assert_equal(['a by'], ScramblePattern.new('bya',[2]).descramble)
+    assert_equal(['two bit'], ScramblePattern.new('obtitw',[3,3]).descramble)
   end
   
 end
